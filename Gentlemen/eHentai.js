e2dc6ce76b59e0713130d08ed250f2e10a87b88a
JESSIZUN AIRTHEVA <airtheva@foxmail.com>
2014-01-28 23:30:02 +0800

    First blood.

---
+++ Gentlemen/eHentai.js
@@ -0,0 +1,529 @@
+
+console.log('Hentai!');
+
+var MPath = require('path');
+var MFs = require('fs');
+
+var MRequest = require('request');
+var MCheerio = require('cheerio');
+
+var Sequence = require('sequence').Sequence;
+var Extend = require('node.extend');
+
+var RESOURCE_STATUS_UNKNOWN = 'unknown';
+var RESOURCE_STATUS_DOWNLOADING = 'downloading';
+var RESOURCE_STATUS_DOWNLOADED = 'downloaded';
+
+var DOWNLOAD_TASK_STATUS_UNKNOWN = 'unknown';
+var DOWNLOAD_TASK_STATUS_RUNNING = 'running';
+var DOWNLOAD_TASK_STATUS_STOPPED = 'stopped';
+var DOWNLOAD_TASK_STATUS_ABORTED = 'aborted';
+var DOWNLOAD_TASK_STATUS_FINISHED = 'finished';
+
+var TIMEOUT = 15000;
+
+var UserAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.66';
+var HTTPProxy = '';
+
+var CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+function makeRandomID(size) {
+
+	var randomID = '';
+
+	for(var i = 0; i < size; i++) {
+		randomID += CHARS.charAt(Math.random() * CHARS.length);
+	}
+
+	return randomID;
+
+}
+
+// 处理文件名，在最大保留原始字符的基础上保证不与Windows冲突。
+function handleFilename(filename) {
+
+	return filename.replace(/[\\\/:\*\?"<>\|]/g, '_');
+
+}
+
+// 生成request的参数，动态生成防止属性发生改变。
+function makeRequestOptions(options) {
+
+	var defaults = {};
+
+	var headers = defaults.headers = {};
+
+	headers['User-Agent'] = UserAgent;
+	headers['Cookie'] = 'tips=1; nw=1';
+
+	defaults.proxy = HTTPProxy != '' ? HTTPProxy : null;
+	defaults.encoding = null;
+
+	return Extend(true, {}, defaults, options);
+
+}
+
+function makeResource(options) {
+
+	var defaults = {};
+
+	defaults.type = '';
+	defaults.title = '';
+	defaults.time = '';
+	defaults.previewUri = '';
+	defaults.uri = '';
+
+	// 由extractResource填充。
+	defaults.imageResourceAmount = 0;
+	defaults.firstImageResourceUri = '';
+
+	return Extend(true, {}, defaults, options);
+
+}
+
+function MakeResourceDir(resource) {
+
+	var resourceDir = MPath.join(__dirname, 'downloads', resource.title);
+
+	return resourceDir;
+
+}
+
+function makeDownloadTask(options) {
+
+	var defaults = {};
+
+	defaults.id = '';
+	defaults.resource = {};
+
+	defaults.imageResourceIndex = 0;
+	defaults.progress = 0;
+
+	defaults.status = DOWNLOAD_TASK_STATUS_UNKNOWN;
+	defaults.err = null;
+	defaults.logs = '';
+
+	// 这个是用来停止的。
+	defaults.isStopping = false;
+
+	return Extend(true, {}, defaults, options);
+
+}
+
+// 从首页中提取资源列表。
+function extractResourceList(body) {
+
+	var result = {};
+
+	var $ = MCheerio.load(body);
+
+	var $tds = $('.ptt td');
+
+	result.pageAmount = parseInt($tds.eq($tds.length - 2).text(), 10);
+
+	console.log(result.pageAmount);
+
+	result.resourceList = [];
+
+	var $trs = $('.itg tr[class^=gtr]');
+
+	$trs.each(function(index, tr) {
+
+		var $td = $(tr).find('td');
+
+		var options = {};
+
+		options.type = $td.eq(0).find('img').attr('alt');
+		options.title = handleFilename(decodeURIComponent(encodeURIComponent($td.eq(2).find('.it5 a').text())));
+		options.time = $td.eq(1).text();
+		// 第一个。
+		options.previewUri = $td.eq(2).find('.it2 img').attr('src');
+		// 其它。
+		if(!options.previewUri) {
+			options.previewUri = $td.eq(2).find('.it2').text().replace('init~ehgt.org~', 'http://ehgt.org/').split('~')[0];
+		}
+
+		options.uri = $td.eq(2).find('.it5 a').attr('href');
+
+		result.resourceList.push(makeResource(options));
+
+	});
+
+	return result;
+
+}
+
+// 从资源页中更新与补充资源信息。
+function extractResource(resource, body) {
+
+	var $ = MCheerio.load(body);
+
+	resource.type = $('#gdc a img').attr('alt');
+	resource.title = handleFilename(decodeURIComponent(encodeURIComponent($('#gn').text())));
+	resource.time = $('.gdt2').eq(0).text();
+
+	resource.previewUri = $('#gd1 img').attr('src');
+
+	//resource.uri.
+
+	resource.imageResourceAmount = parseInt($('.gdt2').eq(1).text().split(' ')[0], 10);
+	resource.firstImageResourceUri = $('#gdt a').eq(0).attr('href');
+
+	return resource;
+
+}
+
+// 从图像资源页中提取图像信息。
+function extractImageResource(imageResourceUri, body) {
+
+	var imageResource = {};
+
+	var $ = MCheerio.load(body);
+
+	var $a = $('a img').parent().eq(4);
+
+	imageResource.uri = imageResourceUri;
+	imageResource.imageUri = $a.find('img').attr('src');
+	imageResource.nextUri = $a.attr('href');
+	if(imageResource.nextUri == imageResource.uri) {
+		// 如果下一个地址和这个地址相同，说明已经执行到了末尾。
+		imageResource.nextUri = null;
+	}
+
+	return imageResource;
+
+}
+
+// 通过请求获得图像资源。
+// callback(err, imageResource)
+function getImageResource(refererUri, imageResourceUri, callback) {
+
+		MRequest(makeRequestOptions({
+			'uri': imageResourceUri,
+			'headers': {
+				'Referer': refererUri
+			},
+			'encoding': 'utf-8',
+			'timeout': TIMEOUT
+		}), function(err, res, body) {
+
+			if(err) {
+				callback(err);
+			}
+			else {
+				var imageResource = extractImageResource(imageResourceUri, body);
+				callback(null, imageResource);
+			}
+
+		});
+
+}
+
+// 根据图像资源下载图像。
+// callback(err, imageResource)
+function downloadImage(downloadTask, imageResource, callback) {
+
+	var resource = downloadTask.resource;
+
+	var parts = MPath.basename(imageResource.imageUri).split('.');
+	var extension = parts[parts.length - 1];
+	var filename = MPath.basename(imageResource.uri) + '.' + extension;
+	var path = MPath.join(MakeResourceDir(resource), handleFilename(filename));
+
+	log(downloadTask, '准备下载' + filename + '。');
+
+	MFs.exists(path, function(exists) {
+
+		if(exists) {
+
+			log(downloadTask, filename + '已存在，跳过。');
+			callback(null, imageResource);
+
+		}
+		else {
+
+			log(downloadTask, '开始下载' + filename + '。');
+			MRequest(makeRequestOptions({
+				'uri': imageResource.imageUri,
+				'headers': {
+					'Referer': imageResource.uri
+				}
+			}), function(err, res, buffer) {
+
+				if(err) {
+					callback(err);
+				}
+				else {
+
+					MFs.writeFile(path, buffer, function(err) {
+
+						if(err) {
+							callback(err);
+						}
+						else {
+							log(downloadTask, filename + '下载完成。');
+							callback(null, imageResource);
+						}
+
+					});
+
+				}
+
+			});
+
+		}
+
+	});
+
+}
+
+function log(downloadTask, message) {
+
+	downloadTask.logs += message;
+	downloadTask.logs += '\n';
+
+}
+
+// 通过请求获得资源列表。
+// callback(err, pageAmount, resourceList)
+function GetResourceList(filter, pageIndex, callback) {
+
+	var uri = 'http://g.e-hentai.org/';
+
+	MRequest(makeRequestOptions({
+		'uri': uri,
+		'qs': {
+			'f_search': filter,
+			'page': pageIndex
+		},
+		'encoding': 'utf-8',
+		'timeout': TIMEOUT
+	}), function(err, res, body) {
+
+		if(err) {
+			callback(err);
+		}
+		else {
+			var result = extractResourceList(body);
+			callback(null, result.pageAmount, result.resourceList);
+		}
+
+	});
+
+}
+
+var DownloadTaskList = {};
+
+// 从资源创建下载任务。
+// callback(err, downloadTask)
+function CreateDownloadTaskByResource(resource, callback) {
+
+	var options = {};
+	options.id = makeRandomID(16);
+	options.resource = resource;
+
+	var downloadTask = makeDownloadTask(options);
+
+	MRequest(makeRequestOptions({
+		'url': resource.uri,
+		'encoding': 'utf-8'
+	}), function(err, res, body) {
+		if(err) {
+			callback(err);
+		}
+		else {
+			extractResource(resource, body);
+			DownloadTaskList[downloadTask.id] = downloadTask;
+			callback(null, downloadTask);
+		}
+	});
+
+}
+
+// 从资源地址创建下载任务。
+// callback(err, downloadTask)
+function CreateDownloadTaskByResourceUri(resourceUri, callback) {
+
+	var options = {};
+	options.id = makeRandomID(16);
+	options.resource = makeResource({
+		'uri': resourceUri
+	});
+
+	var downloadTask = makeDownloadTask(options);
+
+	MRequest(makeRequestOptions({
+		'url': downloadTask.resource.uri,
+		'encoding': 'utf-8'
+	}), function(err, res, body) {
+		if(err) {
+			callback(err);
+		}
+		else {
+			extractResource(downloadTask.resource, body);
+			DownloadTaskList[downloadTask.id] = downloadTask;
+			callback(null, downloadTask);
+		}
+	});
+
+}
+
+// 开始下载任务，并向回调汇报当前的downloadTask。
+// callback(err, downloadTask)
+function StartDownloadTask(downloadTaskID, callback) {
+
+	var downloadTask = DownloadTaskList[downloadTaskID];
+
+	var resource = downloadTask.resource;
+
+	var resourceDir = MakeResourceDir(resource);
+
+	downloadTask.isStopping = false;
+
+	// 进入第一张图像资源页。
+	Sequence().then(function(next) {
+
+		log(downloadTask, '准备下载：' + resource.title + '。');
+		log(downloadTask, '准备解析第一张图像资源。');
+
+		getImageResource(resource.uri, resource.firstImageResourceUri, function(err, firstImageResource) {
+
+			if(err) {
+				downloadTask.status = DOWNLOAD_TASK_STATUS_ABORTED;
+				downloadTask.err = err;
+				callback(err, downloadTask);
+			}
+			else {
+				log(downloadTask, '第一张图像资源解析成功。');
+				next(firstImageResource);
+			}
+
+		});
+
+	}).then(function(next, firstImageResource) {
+
+		var callback0 = function(err, imageResource) {
+
+			if(err) {
+				downloadTask.status = DOWNLOAD_TASK_STATUS_ABORTED;
+				downloadTask.err = err;
+				callback(err, downloadTask);
+			}
+			else {
+
+				if(!downloadTask.isStopping) {
+
+					downloadTask.progress = (downloadTask.imageResourceIndex + 1) / resource.imageResourceAmount * 100;
+					downloadTask.status = DOWNLOAD_TASK_STATUS_RUNNING;
+
+					if(imageResource.nextUri != null) {
+
+						log(downloadTask, '正在下载，当前进度为：' + downloadTask.imageResourceIndex + ' / ' + resource.imageResourceAmount + '。');
+						getImageResource(imageResource.uri, imageResource.nextUri, function(err, nextImageResource) {
+
+							if(err) {
+								callback(err, downloadTask);
+							}
+							else {
+
+								downloadImage(downloadTask, nextImageResource, callback0);
+								downloadTask.imageResourceIndex++;
+
+							}
+
+						})
+
+					}
+					else {
+						// 结束。
+						downloadTask.imageResourceIndex = resource.imageResourceAmount - 1;
+						downloadTask.progress = 100;
+						downloadTask.status = DOWNLOAD_TASK_STATUS_FINISHED;
+
+						MFs.unlink(MPath.join(resourceDir, 'downloading'), function(err) {
+
+						});
+
+						MFs.writeFile(MPath.join(resourceDir, 'downloaded'), '', function(err) {
+
+						});
+						log(downloadTask, resource.title + '下载完成。');
+					}
+
+				}
+				else {
+					// 停止。
+					downloadTask.status = DOWNLOAD_TASK_STATUS_STOPPED;
+
+				}
+
+				callback(null, downloadTask);
+
+			}
+
+		};
+
+		// FIXME: Marked. 这是因为懒了所以没异步，这点小东西还异步…
+		try {
+
+			if(!MFs.existsSync(resourceDir)) {
+				MFs.mkdirSync(resourceDir);
+			}
+
+			var json = JSON.stringify(resource, null, 4);
+			MFs.writeFileSync(MPath.join(resourceDir, 'resource.json'), json);
+
+		}
+		catch(err) {
+			console.log(err);
+		}
+
+		MFs.writeFile(MPath.join(resourceDir, 'downloading'), '', function(err) {
+
+		});
+
+		downloadImage(downloadTask, firstImageResource, callback0);
+
+	});
+
+}
+
+function StopDownloadTask(downloadTaskID) {
+
+	var downloadTask = DownloadTaskList[downloadTaskID];
+
+	downloadTask.isStopping = true;
+
+}
+
+function RemoveDownloadTask(downloadTaskID) {
+
+	DownloadTaskList[downloadTaskID] = null;
+	delete DownloadTaskList[downloadTaskID];
+
+}
+
+// 获得代理服务器。
+function GetHTTPProxy() {
+
+	return HTTPProxy;
+
+}
+
+// 设置代理服务器。
+function SetHTTPProxy(httpProxy) {
+
+	HTTPProxy = httpProxy;
+
+}
+
+exports.GetResourceList = GetResourceList;
+exports.MakeResourceDir = MakeResourceDir;
+exports.CreateDownloadTaskByResource = CreateDownloadTaskByResource;
+exports.CreateDownloadTaskByResourceUri = CreateDownloadTaskByResourceUri;
+exports.StartDownloadTask = StartDownloadTask;
+exports.StopDownloadTask = StopDownloadTask;
+exports.RemoveDownloadTask = RemoveDownloadTask;
+
+exports.GetHTTPProxy = GetHTTPProxy;
+exports.SetHTTPProxy = SetHTTPProxy;
--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-gentlemen
-=========
+# Gentlemen
+
+Gentlemen是一个E绅士的客户端，使用node-webkit作为宿主环境，在前端使用了avalon与bootstrap，是一次对avalon的尝试，期间踩了挺多的坑，但最后证明avalon还是能用的。
+
+## TODO
+
+* 下次启动时恢复下载任务。
+* 下载任务详细信息的实时更新。
+* 配置的预加载。
+
+## 前端
+
+前端使用了avalon和bootstrap，avalon负责应用结构，bootstrap负责界面美化。
+
+本来打算只用bootstrap.css，但后来发现要实现modal还是挺麻烦的，最后还是引入了bootstrap.js。
+
+应用结构如下：
+
+* Application - 主框架，控制标签显示。
+** Browse - 浏览，展示与收藏资源。
+** Collect - 收藏，将资源收藏以后才可以进行其它的操作。
+** Download - WIP - 下载，展示下载列表。
+** Configure - WIP- 配置，你懂的。
+** AddDownloadTaskByResourceUriDialog - 从资源地址添加下载任务的对话框。
+** DownloadTaskDetailDialog - 下载任务详细信息的对话框。
+
+## 后端
+
+后端主要使用了request模块用来简化HTTP请求的发送，还有cheerio模块提供类jQuery语法快速解析HTML源代码。
+
+* 私有
+** 工具函数
+*** String makeRandomID(int size);
+*** String handleFilename(String filename);
+** 工厂函数
+*** Object makeRequestOptions(Object options);
+*** Resource makeResource(Object options);
+*** DownloadTask makeDownloadTask(Object options);
+** 数据获取与处理函数
+*** Resource[] extractResourceList(String body);
+*** Resource extractResource(Resource resource, String body);
+*** ImageResource extractImageResource(String imageResourceUri, String body);
+*** void getImageResource(String refererUri, String imageResourceUri, Function(Object err, ImageResource imageResource) callback);
+*** void downloadImage(DownloadTask downloadTask, ImageResource imageResource, Function(Object err, ImageResource imageResource) callback);
+** 日志函数
+*** String log(DownloadTask downloadTask, String message);
+
+* 公开
+** 浏览接口
+*** void GetResourceList(String filter, int pageIndex, Function(Object err, int pageAmount, Resource[] resourceList) callback);
+*** String MakeResourceDir(Resource resource);
+** 下载接口
+*** void CreateDownloadTaskByResource(Resource resource, Function(Object err, DownloadTask downloadTask) callback);
+*** void CreateDownloadTaskByResourceUri(String resourceUri, Function(Object err, DownloadTask downloadTask) callback);
+*** void StartDownloadTask(String downloadTaskID, Function(Object err, DownloadTask downloadTask) callback);
+*** void StopDownloadTask(String downloadTaskID);
+*** void RemoveDownloadTask(String downloadTaskID);
+** 配置接口
+*** String GetHTTPProxy();
+void SetHTTPProxy(String httpProxy);
+
+## 协议
+
+Copyright (C) 2014 airtheva
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
